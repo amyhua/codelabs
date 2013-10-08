ef6819488f4f24d3634671d4bbcdd6918ce44070
Amy Hua <foramyhua@gmail.com>
2013-10-08 12:39:00 -0700

    initializers

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Collaboration::Application.config.session_store :cookie_store, :key => '_collaboration_session3'
+Collaboration::Application.config.session_store :cookie_store, :key => 'asdf7af70f97as0df9aw7g0dfn7djd7t0jd8rths9drh7drt09kjdtrmfhfyu9k70j9ergt23l5kj2362j6532h73h6354h23o435t4tregngjlg'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

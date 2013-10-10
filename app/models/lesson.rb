b3ca2f93ac2ce731ed1f5c818c27c0a820bb82e9
Amy Hua <foramyhua@gmail.com>
2013-10-09 19:02:35 -0700

    Upcoming lessons includes today

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ActiveRecord::Base
   default_scope order('date')
   scope :published, where(status: 'published')
-  scope :upcoming, where('date >= ?', Time.now)
+  scope :upcoming, where('date > ?', Time.now.yesterday)
   scope :past, where('date < ?', Time.now)
   attr_accessible :content, :date, :description, :name, :user_id, :status
   has_many :codepens
